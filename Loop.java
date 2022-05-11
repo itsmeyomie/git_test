456d84778decba12f66b13e6a23a4733645df421
itsmeyomie <40064353+itsmeyomie@users.noreply.github.com>
2022-05-11 13:43:35 +0300

    Add files via upload
    added java file

---
+++ Loop.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package loop;
+
+/**
+ *
+ * @author yomie
+ */
+public class Loop {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("i will make it"); 
+        }
+    }
+    
+}
